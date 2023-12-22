e29ba399bc78119c2a504cb5ff8ee45d2bb7e8b8
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2023-12-22 13:49:15 +0300

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MainLayoutComponent } from './modules/main-layout/components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MainLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/main-layout/main-layout-routing.module.ts
+++ src/app/modules/main-layout/main-layout-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'layout', component: MainLayoutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
