56f9e45bd886500e3800d14561f00f8a3138193b
shaphankirui <79963010+shaphankirui@users.noreply.github.com>
2024-01-10 15:34:04 +0300

    menu

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     path: '', // This should be an empty path as it's already part of the parent route in AppRoutingModule
     component: DashbordMainComponent,
     children: [
-      { path: 'menuu', component: DashbordMainComponent }, // Route for MainMenuComponent within MLayoutComponent
+      { path: '', component: DashbordMainComponent }, // Route for MainMenuComponent within MLayoutComponent
       // Add more child routes here if needed for other components within MLayoutComponent
     ]
   },
